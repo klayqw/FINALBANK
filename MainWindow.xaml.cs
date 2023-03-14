4d3f0e87ae7708cf4e6411f888744e74d3313564
klayqw <akasiyapika@gmail.com>
2023-03-14 23:43:51 +0400

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bank
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var temp2 = File.ReadAllText("User Base/USERBASE.json");
+            var list = JsonSerializer.Deserialize<List<User>>(temp2);
+            var user = list.Find(x => x.Nickname == "klay");
+           // new Main(user).Show();
+            if (Directory.Exists("User Base") == false)
+            {
+                Directory.CreateDirectory("User Base");
+                var temp = new List<User>();
+                var json = JsonSerializer.Serialize(temp);
+                File.WriteAllText("User Base/USERBASE.json", json);
+            }
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            var json = File.ReadAllText("User Base/USERBASE.json");
+            var list = JsonSerializer.Deserialize<List<User>>(json);
+
+            if (list.Any(x => x.Nickname == txtUser.Text))
+            {
+                var user = list.Find(x => x.Nickname == txtUser.Text);
+                if (user.Nickname == txtUser.Text || user.Password == txtPass.Password)
+                {
+                    MessageBox.Show("Welcome " + user.Nickname + "!", "Welcome", MessageBoxButton.OK, MessageBoxImage.Information);
+                    var mainwin = new Main(user);
+                    mainwin.Show();
+                    this.Close();
+                    return;
+                }
+            }
+            MessageBox.Show("User not found", "LOG ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Window_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void btnReg_Click(object sender, RoutedEventArgs e)
+        {
+            //new Registation().ShowDialog();
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            //new Forgot().ShowDialog();
+        }
+
+    }
+}
---
+++ User.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank;
+
+public class User : INotifyPropertyChanged
+{
+ 
+    public string? Nickname {
+        get => nickname;
+        set
+        {
+            if (value == null)
+            {
+                nickname = null;
+            }
+            if(value.Length <= 0 || value.Length >= 12)
+            {
+                throw new Exception("Value too small or too big");
+            }
+            nickname = value;
+            OnPropertyChanged(nameof(Nickname));
+        }
+    }
+    private string? nickname;
+
+    public string? Password {
+        get => password;
+        set
+        {
+            if (value == null)
+            {
+                password = null;
+            }
+            if (value.Length <= 0  || value.Length < 4 || value.Length >= 16)
+            {
+                throw new Exception("Value too small or too big");
+            }
+            password = value;
+            OnPropertyChanged(nameof(Password));
+        }
+             
+    }
+    private string? password;
+
+    public string? Firstname 
+    {
+        get => firstname;
+        set
+        {
+            if (value == null)
+            {
+                firstname = null;
+            }
+            if (value.Length <= 0 || value.Length < 3  || value.Length >= 16)
+            {
+                throw new Exception("Value too small or too big");
+            }
+            firstname = value;
+            OnPropertyChanged(nameof(Firstname));
+        }
+    }
+    private string? firstname;
+    public string? Lastname {
+        get => lastname;
+
+        set
+        {
+            if (value == null)
+            {
+                lastname = null;
+            }
+            if (value.Length <= 0 || value.Length < 6 || value.Length >= 18)
+            {
+                throw new Exception("Value too small or too big");
+            }
+            lastname = value;
+            OnPropertyChanged(nameof(Lastname));
+        }
+    }
+    private string? lastname;
+    public string? Creditcard
+    {
+        get => creditcard;
+        set 
+        {
+            if (value == null)
+            {
+                creditcard = null;
+            }
+            if (value.Length != 16)
+            {
+                throw new Exception("Value too small or too big");
+            }
+            creditcard = value;
+            OnPropertyChanged(nameof(Creditcard));
+        }
+    }
+
+    private string? creditcard;
+    public int? Id {
+        get => id;
+        set
+        {
+            id = value;
+            OnPropertyChanged(nameof(Id));
+        }
+    }
+    private int? id;
+    public int? Cvv 
+    {
+        get => cvv;
+        set
+        {
+            if (value == null)
+            {
+                cvv = null;
+            }
+            cvv = value;
+            OnPropertyChanged(nameof(Cvv));
+        }
+    }
+    private int? cvv;
+    public DateTime? Creditcarddate 
+    {
+        get => creditcarddate;
+        set
+        {
+            if(value == null)
+            {
+                creditcarddate = null;
+            }
+            if(value.Value.Year < DateTime.Now.Year || value.Value.Year > DateTime.Now.Year + 7)
+            {
+                throw new Exception("Value too small or too big");
+            }
+            creditcarddate = value;
+            OnPropertyChanged(nameof(Creditcarddate));
+        }
+    }
+    private DateTime? creditcarddate;
+    public DateTime? Birthday 
+    {
+        get => birthday;
+        set
+        {
+            if (value == null)
+            {
+                birthday = null;
+            }
+            if (value.Value.Year < 1970)
+            {
+                throw new Exception("Value null");
+            }
+            birthday = value;
+            OnPropertyChanged(nameof(Birthday));
+        }
+    }
+    private DateTime? birthday;
+    public string? Phone
+    {
+        get => phone;
+        set
+        {
+            if (value == null)
+            {
+                phone = null;
+            }
+            if (value.Length <= 0  || value.Length >= 12)
+            {
+                throw new Exception("Value too small or too big");
+            }
+            phone = value;
+            OnPropertyChanged(nameof(Phone));
+        }
+    }
+    private string? phone;
+    public double? Balance 
+    {
+
+        get => balance;
+        set
+        {
+            if (value == null)
+            {
+                balance = null;
+            }
+            if(value >= double.MaxValue)
+            {
+                throw new Exception("Value too small or too big");
+            }
+            balance = value;
+            OnPropertyChanged(nameof(Balance));
+        }
+    }
+    public double? balance;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged(string propertyName)
+    {
+        var handler = PropertyChanged;
+        if (handler != null)
+            handler(this, new PropertyChangedEventArgs(propertyName));
+
+        // With C# 6 this can be replaced with
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
